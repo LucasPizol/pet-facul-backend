d97fdacbe213541170fbe278b99ac70382b173e2
Lucas Pizol Ferreira <lucaspizolfe@gmail.com>
2024-05-12 09:53:37 -0300

    feat: register use controller

---
+++ src/controllers/user/register-user/register-user.ts
@@ -0,0 +1,37 @@
+import { IAddUserModel } from "@/domain/models/user";
+import { IRegisterUser } from "@/domain/use-cases/user/register-user";
+import { badRequest, created, serverError } from "@/main/helpers/http";
+import { IController } from "@/main/protocols/controller";
+import { IHttpRequest, IHttpResponse } from "@/main/protocols/http";
+import { validateBodyFields } from "@/utils/validate-body-fields";
+
+export class IRegisterUserController implements IController {
+  private readonly registerUserUseCase: IRegisterUser;
+
+  constructor(registerUserUseCase: IRegisterUser) {
+    this.registerUserUseCase = registerUserUseCase;
+  }
+
+  async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+    try {
+      const user = httpRequest.user;
+
+      if (!user) return badRequest(new Error("user"));
+
+      const data = validateBodyFields<IAddUserModel>(
+        [
+          { key: "name", required: true, type: "string" },
+          { key: "username", required: true, type: "string" },
+          { key: "password", required: true, type: "string" },
+        ],
+        httpRequest.body
+      );
+
+      const response = await this.registerUserUseCase.register(data);
+
+      return created(response);
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}
